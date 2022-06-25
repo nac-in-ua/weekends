2122ab7a5377176dda070f4dda9048e7ce3b690c
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-25 22:47:43 +0300

    added main elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Header from './components/Header/Header'
+import Description from './components/Main/Description'
+import Footer from './components/Main/Footer'
+import Timer from './components/Main/Timer'
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="mt-24 text-center text-5xl font-thin">
-        Weekends countdown
-      </h1>
+    <div className="flex h-screen flex-col bg-white text-slate-600 dark:bg-zinc-900 dark:text-gray-400">
+      <Header />
+      <main className="mb-auto flex flex-col">
+        <Description />
+        <Timer />
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header
+      data-testid="header"
+      className="my-2 mx-4 flex h-12 flex-row justify-between"
+    >
+      header
+    </header>
+  )
+}
+
+export default Header
