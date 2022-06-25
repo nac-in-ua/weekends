0c9febb3cab96f0d1d4ced930fe9a2970e24eb00
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-25 22:55:42 +0300

    added test for main components

---
+++ src/components/Main/__tests__/Footer.test.tsx
@@ -0,0 +1,17 @@
+import Footer from '../Footer'
+import { render, screen } from '@testing-library/react'
+
+describe('Footer', () => {
+  it('should render link with text', () => {
+    render(<Footer />)
+    const footerLink = screen.getByRole('link')
+    expect(footerLink).toBeInTheDocument()
+    expect(footerLink).toMatchSnapshot()
+  })
+  it('should has attributes', () => {
+    render(<Footer />)
+    const footerLink = screen.getByRole('link')
+    expect(footerLink).toHaveAttribute('href', 'https://nac.in.ua')
+    expect(footerLink).toHaveAttribute('target', '_blank')
+  })
+})
