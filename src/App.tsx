7a99313b1a8b753fe8959fe4f01c877e4b0cbe59
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-25 18:07:08 +0300

    fixes by prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl font-thin text-center mt-24">Weekends countdown</h1>
+      <h1 className="text-5xl font-thin text-center mt-24">
+        Weekends countdown
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
