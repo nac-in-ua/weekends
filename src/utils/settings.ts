0ba63c0681f7f75fef331c0a25198cac1a672cda
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-25 23:21:21 +0300

    added settings module

---
+++ src/utils/settings.ts
@@ -0,0 +1,70 @@
+interface IFriday {
+  day: number
+  hour: number
+}
+
+interface IDEFAULT_FRIDAY {
+  day: number
+  hour: number
+  minute: number
+  second: number
+}
+
+interface IGetDefaultSettings {
+  greetingsText: string
+  day: number
+  hour: number
+  theme: string
+  useSystemTheme: boolean
+  isFirstLoad: boolean
+}
+
+interface IGetTimerData {
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+type SystemTheme = 'dark' | 'light'
+
+const DEFAULT_FRIDAY: IDEFAULT_FRIDAY = {
+  day: 5,
+  hour: 18,
+  minute: 0,
+  second: 0,
+}
+
+const DEFAULT_THEME: string = 'light'
+
+const getDefaultSettings = (): IGetDefaultSettings => ({
+  greetingsText: 'Have a beer!',
+  day: DEFAULT_FRIDAY.day,
+  hour: DEFAULT_FRIDAY.hour,
+  theme: DEFAULT_THEME,
+  useSystemTheme: false,
+  isFirstLoad: true,
+})
+
+const getTimerData = (friday: IFriday): IGetTimerData => ({
+  days: friday.day,
+  hours: friday.hour,
+  minutes: DEFAULT_FRIDAY.minute,
+  seconds: DEFAULT_FRIDAY.second,
+})
+
+const getSystemTheme = (): SystemTheme =>
+  window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+
+const applyTheme = (theme: SystemTheme): void => {
+  const root = document.documentElement
+  if (theme === 'dark') {
+    root.classList.add('dark')
+  } else {
+    root.classList.remove('dark')
+  }
+}
+
+export { getTimerData, getDefaultSettings, getSystemTheme, applyTheme }
