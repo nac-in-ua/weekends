74c259910a5f85375047177ca26ee697b47d7f43
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-26 14:09:55 +0300

    implemented Timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import Header from './components/Header/Header'
 import { Description, Footer, Greetings } from './components/Main'
 import Timer from './components/Main/Timer'
+import { useState, useEffect } from 'react'
+import { applyTheme } from './utils/settings'
+import { useSettingsData } from './store/Settings'
 
 function App() {
+  const [isFinished, setIsFinished] = useState(false)
+  const settings = useSettingsData()
+  // const dispatch = useSettingsDispatch()
+  const [isFirstLoad] = useState(settings.isFirstLoad)
+
+  // const handleInitialSettings = (data) => {
+  //   setIsFirstLoad(false)
+  //   dispatch({
+  //     type: 'set',
+  //     payload: { ...data, isFirstLoad: false },
+  //   })
+  // }
+
+  useEffect(() => {
+    applyTheme(settings.theme)
+  }, [settings.theme])
+
   return (
     <div className="flex h-screen flex-col bg-white text-slate-600 dark:bg-zinc-900 dark:text-gray-400">
       <Header />
       <main className="mb-auto flex flex-col">
         <Description />
-        <Timer />
-        <Greetings text="have a beer!" />
+        {!isFinished && !isFirstLoad && <Timer onFinish={setIsFinished} />}
+        {isFinished && <Greetings text={settings.greetingsText} />}
       </main>
       <Footer />
     </div>
--- src/components/Main/Timer.tsx
+++ src/components/Main/Timer.tsx
@@ -1,10 +1,29 @@
+import Clock from '../Clock/Clock'
 import Actiontext from './Actiontext'
+import { useEffect } from 'react'
+import { useSettingsData } from '../../store/Settings'
+import { getTimerData } from '../../utils/settings'
+import { isFinished } from '../../utils/getTimeLeft'
+import useCountdown from '../../hooks/useCountdown'
 
-function Timer() {
+type TimerProps = {
+  onFinish: Function
+}
+
+function Timer({ onFinish }: TimerProps) {
+  const settings = useSettingsData()
+  const time = useCountdown(getTimerData(settings))
+
+  useEffect(() => {
+    if (isFinished(time)) {
+      onFinish(true)
+    }
+  }, [time, onFinish])
+
   return (
     <div>
       <Actiontext />
-      <div>timer</div>
+      <Clock time={time} />
     </div>
   )
 }
