7ae384b08216a9d5b3fc3ecc0dd2fc10d073e025
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-09 01:14:11 +0300

    removed playwright ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'cypress'
 import * as dotenv from 'dotenv'
+
 dotenv.config()
 
 export default defineConfig({
   e2e: {
     baseUrl: process.env.SITE_URL || 'http://localhost:3121',
   },
+  projectId: 'chg2fj',
 })
