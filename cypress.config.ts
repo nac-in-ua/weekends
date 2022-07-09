0f5dcecbf057ce4b044405e22a91ddeab554dd3d
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-10 00:02:28 +0300

    ci changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   projectId: process.env.CYPRESS_PROJECT_ID || '',
   e2e: {
     baseUrl: process.env.SITE_URL || 'http://localhost:3121',
+    fixturesFolder: false,
+    supportFile: false,
   },
+  video: false,
 })
