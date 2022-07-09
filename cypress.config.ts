682f4cad695817085c44a26935c800f9b40fe59e
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-09 14:33:59 +0300

    added projectId

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
+  projectId: 'chg2fj',
   e2e: {
     baseUrl: process.env.SITE_URL || 'http://localhost:3121',
   },
-  projectId: process.env.CYPRESS_PROJECT_ID || '',
 })
