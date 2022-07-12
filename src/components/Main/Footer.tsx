4c815e25407f8c0e8296123f1462d0c16a92662b
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-07-12 03:39:58 +0300

    reverted footer dev text

--- src/components/Main/Footer.tsx
+++ src/components/Main/Footer.tsx
@@ -1,6 +1,12 @@
 function Footer() {
   return (
     <footer className="mb-2 flex flex-col items-center justify-center text-center text-base font-thin">
+      {process.env.NODE_ENV !== 'production' && (
+        <small>
+          You are running this application in <b>{process.env.NODE_ENV}</b>{' '}
+          mode.
+        </small>
+      )}
       <a className="flex" href="mailto: nac.in.ua@gmail.com">
         nac.in.ua
       </a>
